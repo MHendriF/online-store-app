648c076f7a9a5d43b4de0d60abf64ae8991cf388
MHendriF <hendrifebriansyah28@gmail.com>
2024-05-16 16:18:38 +0700

    feat: delete cart

--- src/components/views/cart/index.tsx
+++ src/components/views/cart/index.tsx
@@ -1,20 +1,38 @@
 import { Product } from "@/types/product.type";
 import styles from "./Cart.module.scss";
-import { Dispatch, Fragment, SetStateAction } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import Image from "next/image";
 import { convertToRupiah } from "@/utils/currency";
 import Select from "@/components/ui/Select";
 import Input from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
+import userServices from "@/services/user";
+import { ToasterContext } from "@/contexts/ToasterContext";
+import { ToasterType } from "@/types/toaster.type";
+import productServices from "@/services/product";
 
-type PropTypes = {
-  cart: any;
-  products: Product[];
-  setToaster: Dispatch<SetStateAction<{}>>;
-};
+export default function CartView() {
+  const { setToaster }: ToasterType = useContext(ToasterContext);
+  const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-export default function CartView(props: PropTypes) {
-  const { cart, products, setToaster } = props;
+  const getAllProducts = async () => {
+    const { data } = await productServices.getAllProducts();
+    setProducts(data.data);
+  };
+
+  const getCart = async () => {
+    const { data } = await userServices.getCart();
+    setCart(data.data);
+  };
+
+  useEffect(() => {
+    getCart();
+  }, []);
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
 
   const getProduct = (id: string) => {
     return products.find((item: Product) => item.id === id);
@@ -48,72 +66,108 @@ export default function CartView(props: PropTypes) {
     return total;
   };
 
+  const handleDeleteCart = async (id: string, size: string) => {
+    const newCart = cart.filter((item: { id: string; size: string }) => {
+      return item.id !== id || item.size !== size;
+    });
+    try {
+      const result = await userServices.addToCart({ carts: newCart });
+      if (result.status === 200) {
+        setCart(newCart);
+        setToaster({
+          variant: "success",
+          message: "Success delete item from cart",
+        });
+      }
+    } catch (error) {
+      setToaster({
+        variant: "danger",
+        message: "Failed delete item from cart",
+      });
+    }
+  };
+
   return (
     <div className={styles.cart}>
       <div className={styles.cart__main}>
         <h1 className={styles.cart__main__title}>Cart</h1>
-        <div className={styles.cart__main__list}>
-          {cart.map((item: { id: string; size: string; qty: number }) => (
-            <Fragment key={`${item.id}-${item.size}`}>
-              <div className={styles.cart__main__list__item}>
-                {getProduct(item.id)?.image && (
-                  <Image
-                    src={`${getProduct(item.id)?.image}`}
-                    width={150}
-                    height={150}
-                    alt={`${item.id}-${item.size}`}
-                    className={styles.cart__main__list__item__image}
-                  />
-                )}
-                <div className={styles.cart__main__list__item__info}>
-                  <h4 className={styles.cart__main__list__item__info__title}>
-                    {getProduct(item.id)?.name}
-                  </h4>
-                  <p className={styles.cart__main__list__item__info__categoory}>
-                    {getProduct(item.id)?.category}
-                  </p>
+        {cart.length > 0 ? (
+          <div className={styles.cart__main__list}>
+            {cart.map((item: { id: string; size: string; qty: number }) => (
+              <Fragment key={`${item.id}-${item.size}`}>
+                <div className={styles.cart__main__list__item}>
+                  {getProduct(item.id)?.image && (
+                    <Image
+                      src={`${getProduct(item.id)?.image}`}
+                      width={150}
+                      height={150}
+                      alt={`${item.id}-${item.size}`}
+                      className={styles.cart__main__list__item__image}
+                    />
+                  )}
+                  <div className={styles.cart__main__list__item__info}>
+                    <h4 className={styles.cart__main__list__item__info__title}>
+                      {getProduct(item.id)?.name}
+                    </h4>
+                    <p
+                      className={styles.cart__main__list__item__info__categoory}
+                    >
+                      {getProduct(item.id)?.category}
+                    </p>
 
-                  <div className={styles.cart__main__list__item__info__data}>
-                    <label
-                      className={
-                        styles.cart__main__list__item__info__data__size
-                      }
-                    >
-                      Size
-                      <Select
-                        name="size"
-                        options={getOptionsSize(item.id, item.size)}
-                      />
-                    </label>
-                    <label
-                      className={styles.cart__main__list__item__info__data__qty}
-                    >
-                      Quantity
-                      <Input
-                        name="qty"
-                        type="number"
+                    <div className={styles.cart__main__list__item__info__data}>
+                      <label
                         className={
-                          styles.cart__main__list__item__info__data__qty__input
+                          styles.cart__main__list__item__info__data__size
                         }
-                        defaultValue={item.qty}
-                      />
-                    </label>
+                      >
+                        Size
+                        <Select
+                          name="size"
+                          options={getOptionsSize(item.id, item.size)}
+                          disabled
+                        />
+                      </label>
+                      <label
+                        className={
+                          styles.cart__main__list__item__info__data__qty
+                        }
+                      >
+                        Quantity
+                        <Input
+                          name="qty"
+                          type="number"
+                          className={
+                            styles.cart__main__list__item__info__data__qty__input
+                          }
+                          defaultValue={item.qty}
+                          disabled
+                        />
+                      </label>
+                    </div>
+                    <button
+                      onClick={() => handleDeleteCart(item.id, item.size)}
+                      type="button"
+                      className={styles.cart__main__list__item__info__delete}
+                    >
+                      <i className="bx bxs-trash" />
+                    </button>
                   </div>
-                  <button
-                    type="button"
-                    className={styles.cart__main__list__item__info__delete}
-                  >
-                    <i className="bx bxs-trash" />
-                  </button>
+                  <h4 className={styles.cart__main__list__item__price}>
+                    {convertToRupiah(getProduct(item.id)?.price)}
+                  </h4>
                 </div>
-                <h4 className={styles.cart__main__list__item__price}>
-                  {convertToRupiah(getProduct(item.id)?.price)}
-                </h4>
-              </div>
-              <hr className={styles.cart__main__list__divider} />
-            </Fragment>
-          ))}
-        </div>
+                <hr className={styles.cart__main__list__divider} />
+              </Fragment>
+            ))}
+          </div>
+        ) : (
+          <div className={styles.cart__main__empty}>
+            <h1 className={styles.cart__main__empty__title}>
+              Your cart is empty
+            </h1>
+          </div>
+        )}
       </div>
       <div className={styles.cart__summary}>
         <h1 className={styles.cart__summary__title}>Summary</h1>
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,40 +1,13 @@
 import CartView from "@/components/views/cart";
-import { ToasterContext } from "@/contexts/ToasterContext";
-import productServices from "@/services/product";
-import userServices from "@/services/user";
-import { ToasterType } from "@/types/toaster.type";
 import Head from "next/head";
-import { useContext, useEffect, useState } from "react";
 
 export default function CartPage() {
-  const { setToaster }: ToasterType = useContext(ToasterContext);
-  const [cart, setCart] = useState([]);
-  const [products, setProducts] = useState([]);
-
-  const getAllProducts = async () => {
-    const { data } = await productServices.getAllProducts();
-    setProducts(data.data);
-  };
-
-  const getCart = async () => {
-    const { data } = await userServices.getCart();
-    setCart(data.data);
-  };
-
-  useEffect(() => {
-    getCart();
-  }, []);
-
-  useEffect(() => {
-    getAllProducts();
-  }, []);
-
   return (
     <>
       <Head>
         <title>Cart</title>
       </Head>
-      <CartView cart={cart} products={products} setToaster={setToaster} />
+      <CartView />
     </>
   );
 }
